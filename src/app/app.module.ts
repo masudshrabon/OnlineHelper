48f6dd45626f207b736040ef28c445515dcc942c
masudshrabon <shrabon_bit0413@yahoo.com>
2018-12-07 00:09:28 +0600

    initail commit: added basic pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './users/user/user.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AssignmentFormComponent } from './assignment-form/assignment-form.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'assignment', component: AssignmentFormComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:id', component: UserComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    UsersComponent,
+    UserComponent,
+    AssignmentFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
